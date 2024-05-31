114fdc128ceb6b57a24bed318ec9b0cc2579433d
Divyanshu Rai <160017461+divyanshu2209@users.noreply.github.com>
2024-05-31 13:53:45 +0000

    some basic operations in js

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -28,5 +28,27 @@ let valueInBoolean = Boolean(isLoggedIn)
 
 let someNumber = 22
 let valueInString = String(someNumber)
-console.log(valueInString);
-console.log(typeof(valueInString));
+// console.log(valueInString);
+// console.log(typeof(valueInString));
+
+
+// ----------------- Operations Part -----------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Divyanshu!"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2); //string first hai to string mai hoga
+
+// console.log(1 + 2 + "2"); //number first hai to number mai hoga
+
+
+
