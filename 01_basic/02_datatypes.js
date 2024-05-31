bab94e0aad8b3c815b76199c9adf79a794b981fd
Divyanshu Rai <160017461+divyanshu2209@users.noreply.github.com>
2024-05-31 13:12:18 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readibility should be high
+
+console.log("Divyanshu")
+
+
+let name = "Divyanshu"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined
+// symbol => unique (main implementation in react.js)
+
+
+//object
+
+console.log(typeof "hitesh"); //string
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
