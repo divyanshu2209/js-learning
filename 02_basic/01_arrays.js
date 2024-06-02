935e076da80c5d701d7339f4efe0fa7ac41ed8ce
Divyanshu Rai <160017461+divyanshu2209@users.noreply.github.com>
2024-06-02 07:05:43 +0000

    basics of array part 1

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+/*
+shallow copy => it shares some reference point and it is used in JavaScript array/
+
+deep copy => it does not share same reference point.
+*/
+
+const myHeroes = ["ironman", "captain-ameria"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) // used to insert in first place (!should not be used as it is time consuming)
+// myArr.shift() //removes first element
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join() // changes to a string
+
+// console.log(myArr); 
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
