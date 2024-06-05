18b1d1ad26dd3fb6dda33ee3252706569b408792
Divyanshu Rai <160017461+divyanshu2209@users.noreply.github.com>
2024-06-05 08:38:01 +0000

    function with objects and array

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -34,4 +34,37 @@ function loginUserMessage(username = "sam") {
 }
 
 // console.log(loginUserMessage("Divyanshu"))
-console.log(loginUserMessage("Raju"));
+// console.log(loginUserMessage("Raju"));
+
+
+// ...variable is a rest operator (which combines parameters) 
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500)); // [ 500 ] 
+
+
+const user = {
+    username : "divyanshu",
+    price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+// handleObject({
+//     username : "raju",
+//     price : 200
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    console.log(getArray[1]); 
+}
+
+returnSecondValue(myNewArray)
